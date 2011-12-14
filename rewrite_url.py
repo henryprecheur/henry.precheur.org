bf15ca431069f551a1efba75dba2bccd89f614ce
Henry Precheur <henry@precheur.org>
2011-12-14 08:38:49 -0800

    Rewrite URLs, still WIP

---
+++ rewrite_url.py
@@ -0,0 +1,56 @@
+import io
+import re
+import xml.etree
+import html5lib
+
+def absolute(url):
+    '''
+    >>> absolute('foo')
+    False
+    >>> absolute('http://foo')
+    True
+    >>> absolute('//bar')
+    True
+    >>> absolute('://bar') # Invalid
+    False
+    '''
+    return bool(re.match(r'(?i)^([a-z]+:)?//', url))
+
+def rewrite_urls(input, output):
+    '''
+    `input` & `output` are 2 file like objects.
+    '''
+    doc = html5lib.parse(input,
+                         treebuilder='etree',
+                         namespaceHTMLElements=False)
+
+    for attr in ('href', 'src', 'data', 'codebase'):
+        for x in doc.iterfind('.//*[@{}]'.format(attr)):
+            url = x.attrib[attr]
+
+            if absolute(url):
+                continue
+
+            if re.search(r'\bindex\.html\s*$', url):
+                u = url[:-len('index.html')]
+                x.attrib[attr] = u if u else '/'
+            elif re.search(r'\b\.html\s*$', url):
+                x.attrib[attr] = url[:-len('.html')]
+
+    walker = html5lib.treewalkers.getTreeWalker('etree', xml.etree.ElementTree)
+    s = html5lib.serializer.HTMLSerializer()
+
+    # html5lib doesn't serialize the doctype ...
+    output.write(u'<!DOCTYPE html>\n')
+    for text in s.serialize(walker(doc), encoding='utf-8'):
+        output.write(text)
+
+__all__ = ('rewrite_urls',)
+
+if __name__ == '__main__':
+    from sys import argv
+
+    for filename in argv[1:]:
+        input = io.open(filename)
+        output = io.open(filename + '.NEW', 'w')
+        rewrite_urls(input, output)
